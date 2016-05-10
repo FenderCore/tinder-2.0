e9a91285ca9ec05e884b5acaf38bed00227f6e87
FenderCore <lilmcnessy@hotmail.com>
2016-05-11 10:46:00 +1200

    Login and Register for Android
    -Added login page and register page for the android app to interact with

---
+++ Tinder2.0/src/java/android/Login.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package android;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import ejb.Profile;
+
+/**
+ *
+ * @author Shannon
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   private final char QUOTE = '"';
+   
+    Connection conn;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {  
+        HttpSession session = request.getSession(true);
+        // obtain the values of the form data automatically URL decoded
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        String url= "jdbc:mysql://localhost:3306/tinder";
+        String usernameDB = "root";
+        String passwordDB = "";
+        
+        String message = "";
+        String sql = "";
+        boolean success = false;
+        
+        PrintWriter pw = response.getWriter();
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+          conn = DriverManager.getConnection(url, usernameDB, passwordDB);
+        } catch (Exception e) {
+              //e.printStackTrace(pw);
+              message = e.getMessage();
+          }
+        ResultSet rs;
+        
+        try {
+            Statement stmt = conn.createStatement();
+            sql = "SELECT * FROM account WHERE LOWER(username) = LOWER('" + username + "')";
+            rs = stmt.executeQuery(sql);
+            if(rs.next())
+            {
+                String pass = rs.getString("password");
+                if(pass.equals(password))
+                {
+                    success = true;
+                    message = "0";
+                    //session.setAttribute("logged", "true");
+                    //session.setAttribute("username", rs.getString("username"));
+                    //session.setAttribute("id", rs.getInt("account_id"));
+                    
+                    //session.setAttribute("account", new Profile(rs.getString("full_name"), rs.getInt("age"), rs.getString("sex"), rs.getInt("account_id")));
+                    message += "<br>" + rs.getInt("account_id");
+                    message += "<br>" + rs.getString("full_name");
+                    message += "<br>" + rs.getInt("age");
+                    message += "<br>" + rs.getString("sex");
+                } else {
+                    message = "1";
+                }
+            } else {
+                message = "2";
+            }
+        } catch (Exception e) {
+            //message = e.getMessage();
+            //e.printStackTrace(pw);
+        }
+        
+      // set response headers before returning any message content
+      response.setContentType("text/html");
+      // prepare the content of the response
+      pw.println("<!DOCTYPE HTML PUBLIC " + QUOTE +
+        "-//W3C//DTD HTML 4.0 Transitional//EN" + QUOTE + ">\n" +
+        "<HTML>\n" + "<HEAD>\n" +
+        "<TITLE>Tinder 2.0 - Logged In</TITLE>\n" + "</HEAD>\n" + "<BODY>\n");
+        pw.println(message +
+        "</BODY>\n</HTML>\n");
+      pw.close();
+   }
+   
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {  
+       doGet(request, response);
+   }
+
+   // filter string so that it doesn't contain special HTML characters
+   public static String filter(String text)
+   {  StringBuffer buffer = new StringBuffer();
+      for (int i=0; i<text.length(); i++)
+      {  char c = text.charAt(i);
+         if (c == '<') buffer.append("&lt;");
+         else if (c == '>') buffer.append("&gt;");
+         else if (c == '"') buffer.append("quot;");
+         else if (c == '&') buffer.append("amp;");
+         else buffer.append(c);
+      }
+      return buffer.toString();
+   }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Tinder2.0/src/java/android/Register.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package android;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Shannon
+ */
+public class Register extends HttpServlet {
+
+   private final char QUOTE = '"';
+   
+    Connection conn;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {  
+        // obtain the values of the form data automatically URL decoded
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String name = request.getParameter("name");
+        String age = request.getParameter("age");
+        String sex = request.getParameter("sex");
+
+        String url= "jdbc:mysql://localhost:3306/tinder";
+        String usernameDB = "root";
+        String passwordDB = "";
+        
+        String message = "";
+        boolean usernameTaken = false;
+        String sql = "";
+        
+        PrintWriter pw = response.getWriter();
+        try {
+          Class.forName("com.mysql.jdbc.Driver");
+          conn = DriverManager.getConnection(url, usernameDB, passwordDB);
+        } catch (Exception e) {
+              //e.printStackTrace(pw);
+              message = e.getMessage();
+          }
+        ResultSet rs;
+        
+        try {
+            Statement stmt = conn.createStatement();
+            sql = "SELECT * FROM account WHERE username = '" + username + "'";
+            rs = stmt.executeQuery(sql);
+            if(rs.next())
+            {
+                usernameTaken = true;
+                message = "1";
+            }
+        } catch (Exception e) {
+            //message = e.getMessage();
+            //e.printStackTrace(pw);
+        }
+        
+        if(!usernameTaken)
+        {
+            try {
+            Statement stmt = conn.createStatement();
+            sql = "INSERT INTO account(username, password, full_name, sex, age)" 
+               + " VALUES ('" + username + "', '" + password + "', '" + name + "', '" + sex + "', '" + age + "')";
+            stmt.executeUpdate(sql);
+            message = "0";
+        } catch (Exception e) {
+            //message = e.getMessage();
+            //e.printStackTrace(pw);
+        }
+        }
+      // set response headers before returning any message content
+      response.setContentType("text/html");
+      // prepare the content of the response
+      pw.println("<!DOCTYPE HTML PUBLIC " + QUOTE +
+        "-//W3C//DTD HTML 4.0 Transitional//EN" + QUOTE + ">\n" +
+        "<HTML>\n" + "<HEAD>\n" +
+        "<TITLE>Tinder 2.0 - Registered</TITLE>\n" + "</HEAD>\n" + "<BODY>\n");
+        pw.println(message + "</BODY>\n</HTML>\n");
+      pw.close();
+   }
+   
+   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+   {  
+       doGet(request, response);
+   }
+
+   // filter string so that it doesn't contain special HTML characters
+   public static String filter(String text)
+   {  StringBuffer buffer = new StringBuffer();
+      for (int i=0; i<text.length(); i++)
+      {  char c = text.charAt(i);
+         if (c == '<') buffer.append("&lt;");
+         else if (c == '>') buffer.append("&gt;");
+         else if (c == '"') buffer.append("quot;");
+         else if (c == '&') buffer.append("amp;");
+         else buffer.append(c);
+      }
+      return buffer.toString();
+   }
+
+    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
